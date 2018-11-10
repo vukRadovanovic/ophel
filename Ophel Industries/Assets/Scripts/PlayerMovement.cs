64cb69eaee14629ca1f6bea17a378362a7c60023
dennmar <dm>
2018-11-09 21:09:09 -0800

    Update walking movement and add crouch and run modifiers

--- Ophel Industries/Assets/Scripts/PlayerMovement.cs
+++ Ophel Industries/Assets/Scripts/PlayerMovement.cs
@@ -7,37 +7,38 @@ public class PlayerMovement : MonoBehaviour {
   public float moveForce;
   public Rigidbody2D rb2D;
 
+  private bool holdingCrouch;
+  private bool holdingRun;
+
   // Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		holdingCrouch = false;
+    holdingRun = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {}
+	void Update () {
+    holdingRun = Input.GetKey(KeyCode.LeftShift);
+    holdingCrouch = Input.GetKey(KeyCode.LeftControl);
+  }
 
   // handle physics
   void FixedUpdate() {
-    HandleHorizontal();
-    HandleVertical();
+    HandleMovement();
   }
 
-  // handle horizontal movement
-  void HandleHorizontal() {
-    float horizMoveDir = Input.GetAxis("Horizontal");
-    
-    // Time.deltaTime makes our player move at the same rate regardless of
-    // the computer's frame rate
-    // ForceMode2D.Impulse will make the player move without taking mass into
-    // consideration - i.e. snappy movement
-    rb2D.AddForce(Vector2.right * horizMoveDir * moveForce * Time.deltaTime,
-        ForceMode2D.Impulse);
+  void HandleMovement() {
+    float modMoveForce = moveForce;
+
+    if (holdingCrouch) {
+      modMoveForce = moveForce * 0.5f;
+    }
+    else if (holdingRun) {
+      modMoveForce = moveForce * 1.5f;
+    }
+
+    rb2D.velocity = new Vector2(Input.GetAxis("Horizontal") * modMoveForce,
+        Input.GetAxis("Vertical") * modMoveForce);
   }
 
-  // handle vertical movement
-  void HandleVertical() {
-    float vertMoveDir = Input.GetAxis("Vertical"); 
-
-    rb2D.AddForce(Vector2.up * vertMoveDir * moveForce * Time.deltaTime,
-        ForceMode2D.Impulse);
-  }
 }
