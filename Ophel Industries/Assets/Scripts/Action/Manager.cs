744c53960e509f54dd52ee1a85694d405f0bd3a9
dennmar <dennismariano3@gmail.com>
2019-02-22 19:38:33 -0800

    Fix menu button on click listeners

---
+++ Ophel Industries/Assets/Scripts/Action/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Action {
+  public class Manager : MonoBehaviour {
+
+	  void Awake () { }
+	  void Update () { }
+
+    void Pause() {
+      
+    }
+  }
+}
---
+++ Ophel Industries/Assets/Scripts/Menu/Button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menu {
+  public class Button : MonoBehaviour {
+
+    public UnityEngine.UI.Button button;
+    public string sceneToLoad;
+
+	  void Awake() {}
+
+    void Start() {
+      button.onClick.AddListener(LoadScene);
+    }
+
+	  void Update() { }
+
+    public void LoadScene() {
+      Menu.Manager.Instance.ShowNextScene(sceneToLoad);
+    }
+  }
+}
