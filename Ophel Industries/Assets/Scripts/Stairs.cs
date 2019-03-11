5eb36282f3f23299cc58d53a4879d8c7e3dc4f03
Yayka <33806204+Yayka@users.noreply.github.com>
2019-03-11 06:01:11 +0600

    Stairs work (both up and down)

--- Ophel Industries/Assets/Scripts/Stairs.cs
+++ Ophel Industries/Assets/Scripts/Stairs.cs
@@ -13,16 +13,14 @@ public class Stairs : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name.Equals("Player"))
         {
